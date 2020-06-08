fa3c3b38a5039ecbb94f9f135ef3c793f91746bb
Damon Sweeney <me@damons.email>
2020-06-08 19:30:22 +0100

    Add save wallet to fs functionality

--- src/wallets.rs
+++ src/wallets.rs
@@ -1,5 +1,8 @@
 use std::process::Command;
+use std::fs::{File, write, create_dir};
 
+// Create a PEM formatted RSA pub-priv key pair
+// using openssl genpkey
 pub fn create_wallet() {
     let output = Command::new("openssl")
         .arg("genpkey")
@@ -13,5 +16,42 @@ pub fn create_wallet() {
         .expect("Failed to execute process");
 
     let s = if output.status.success() { String::from_utf8_lossy(&output.stdout) } else { String::from_utf8_lossy(&output.stderr) };
-    println!("{:?}", s);
+
+    match output.status.success() {
+        false => panic!("Failed running process to generate wallet."),
+        true => ()
+    }
+    save_wallet("wallet/wallet.pem", &s);
+}
+
+// Save the wallet to the file system.
+fn save_wallet(file_name: &str, data: &str) -> bool {
+    match create_dir("wallet/") {
+        Err(why) => println!("{}", why),
+        Ok(_) => ()
+    };
+
+    let mut file = match File::create(file_name) {
+        Err(why) => panic!("Couldn't create wallet file: {}", why),
+        Ok(file) => file
+    };
+
+    match write(file_name, data.as_bytes()) {
+        Err(why) => panic!("Couldn't write to created wallet file: {}", why),
+        Ok(_) => ()
+    };
+
+    return true;
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn stub() {
+
+    }
+
 }
