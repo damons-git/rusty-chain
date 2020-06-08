7aea8805657deea69a99383a522c57d8b6d27bc5
Damon Sweeney <me@damons.email>
2020-06-08 23:27:52 +0100

    Add example code of signing and verifying dummy data

--- src/wallets.rs
+++ src/wallets.rs
@@ -1,11 +1,20 @@
+extern crate ring;
+extern crate untrusted;
+
 use std::process::Command;
 use std::fs::{File, write, create_dir, read};
 use std::io::{Error, ErrorKind};
 use ring::{rand, signature};
 
-// Create a PEM formatted RSA pub-priv key pair
+
+pub fn create_wallet() -> () {
+    let key_data: Vec<u8> = create_keyfile();
+    save_keyfile("wallet/priv.der", &key_data);
+}
+
+// Create a DER formatted RSA pub-priv key pair
 // using openssl genpkey
-pub fn create_wallet() {
+pub fn create_keyfile() -> Vec<u8> {
     let output = Command::new("openssl")
         .arg("genpkey")
         .arg("-algorithm")
@@ -19,16 +28,17 @@ pub fn create_wallet() {
         .output()
         .expect("Failed to execute process");
 
-    let key_data = if output.status.success() { &output.stdout } else { &output.stderr };
+    let key_data = if output.status.success() { output.stdout } else { output.stderr };
     match output.status.success() {
         false => panic!("Failed running process to generate wallet."),
         true => ()
     }
-    save_wallet("wallet/wallet.der", key_data);
+
+    return key_data;
 }
 
 // Save the wallet to the file system.
-fn save_wallet(file_name: &str, data: &Vec<u8>) -> bool {
+fn save_keyfile(file_name: &str, data: &Vec<u8>) -> bool {
     match create_dir("wallet/") {
         Err(why) => if Error::last_os_error().kind() != ErrorKind::AlreadyExists {
                 panic!("Failed to create wallet directory: {}", why);
@@ -50,16 +60,47 @@ fn save_wallet(file_name: &str, data: &Vec<u8>) -> bool {
 }
 
 // Load a key file stored on disk into memory.
-pub fn load_wallet() {
-    let key_der = match read("wallet/wallet.der") {
+pub fn load_wallet() -> () {
+    let priv_der = match read("wallet/priv.der") {
         Err(why) => panic!("Failed to read the contents of the key file: {}", why),
         Ok(contents) => contents
     };
 
-    let key_pair = signature::RsaKeyPair::from_der(&key_der);
-    println!("{:?}", key_pair);
-}
+    let output = Command::new("openssl")
+        .arg("rsa")
+        .arg("-in")
+        .arg("wallet/priv.der")
+        .arg("-inform")
+        .arg("DER")
+        .arg("-RSAPublicKey_out")
+        .arg("-outform")
+        .arg("DER")
+        .output()
+        .expect("Failed to execute process");
+    let pub_der = if output.status.success() { output.stdout } else { output.stderr };
 
+    let key_pair = match signature::RsaKeyPair::from_der(&priv_der) {
+        Err(why) => panic!("Failed to parse key file: {}", why),
+        Ok(res) => res
+    };
+
+    let pub_key = signature::UnparsedPublicKey::new(&signature::RSA_PKCS1_2048_8192_SHA256, pub_der);
+
+    let msg: &[u8] = b"abc";
+    let rng = rand::SystemRandom::new();
+    let mut sig = vec![0; key_pair.public_modulus_len()];
+    match key_pair.sign(&signature::RSA_PKCS1_SHA256, &rng, msg, &mut sig) {
+        Err(why) => println!("{}", why),
+        Ok(_) => ()
+    };
+
+    println!("{:?}", sig);
+    // sig[0] = 0;
+    let x = match pub_key.verify(&msg, &sig) {
+        Err(why) => println!("Why: {}", why),
+        Ok(res) => res
+    };
+}
 
 #[cfg(test)]
 mod test {
